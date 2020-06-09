af47146f3acebfe85351a8fa69a925fd2bd01980
Lucas <salle.lucas@hotmail.fr>
2020-06-09 16:41:45 +0200

    deploiement

---
+++ precache-manifest.bddc1b4c0a0fcd4dc873009e953fd58d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "2a31e05c4f33caae4b3a",
+    "url": "/js/chunk-vendors.85de7687.js"
+  },
+  {
+    "revision": "4dbc4a88e47e5222b287",
+    "url": "/js/app.12ad2af6.js"
+  },
+  {
+    "revision": "31881adf3d54d6cd83a0947043a8f4e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a31e05c4f33caae4b3a",
+    "url": "/css/chunk-vendors.854ca467.css"
+  },
+  {
+    "revision": "4dbc4a88e47e5222b287",
+    "url": "/css/app.37339add.css"
+  }
+];
